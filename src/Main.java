5e41b14f21d7236486ba47f61521b937bfe15ec5
Александр <alexst731w@gmail.com>
2023-05-21 23:27:22 +0300

    Исправил ДЗ-7

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,13 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int contribution = 15000;
+        int contribution = 15_000;
         int total = 0;
-        int i = 0;
-        while (total <= 2_459_000) {
-            i = i + 1;
-            total = total + total / 100;
+        int month = 0;
+        while (total < 2_459_000) {
+            month += 1;
             total = total + contribution;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+            System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
         }
     }
 
@@ -93,15 +92,16 @@ public class Main {
 
     public static void task7() {
         System.out.println("Задача 7");
-        for (int i = 3; i <= 31; i = i + 7) {
-            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить еженедельный отчет");
+        for (int firstFriday = 3; firstFriday <= 31; firstFriday = firstFriday + 7) {
+            System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить еженедельный отчет");
         }
     }
 
     public static void task8() {
         System.out.println("Задача 8");
-        for (int year = 0; year < 2100; year = year + 79) {
-            if (year >= 1823 && year < 2100) {
+        int currentYear =1900;
+        for (int year = 0; year < currentYear + 200; year = year + 79) {
+            if (year >= 1823) {
                 System.out.println(year);
             }
         }
